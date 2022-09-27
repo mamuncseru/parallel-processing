6c739bbeaba2b705cd4a68cd0edbcae3cf460e58
mamuncseru <mamun.cseian@gmail.com>
2022-09-27 21:25:01 +0600

    deleted extra code in assignment 1

--- lab_experiments/1.cpp
+++ lab_experiments/1.cpp
@@ -7,10 +7,6 @@
 #include<mpi.h>
 #include<stdio.h>
 
-int sum(int a, int b)
-{
-    return a + b;
-}
 
 int main(int argc, char ** argv)
 {
@@ -33,9 +29,6 @@ int main(int argc, char ** argv)
 
     // Print off a hello world message
     printf("Hello world from processor %s, rank %d out of %d processors\n", processor_name, rank_process, num_process);
-
-    int sum_result = sum(10, 15);
-    printf("Value got from processor %s, rank %d out of %d processors is sum %d\n", processor_name, rank_process, num_process, sum_result);
 
     // Finalize the MPI environment.
     MPI_Finalize();
